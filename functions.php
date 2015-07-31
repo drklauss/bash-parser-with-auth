bc9470da78a58d6007ffcbdaef0a258adab1b94d
Aleksey <dr.klauss@gmail.com>
2015-07-31 16:20:17 +0300

    firrst serious commit

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 include_once 'simple_html_dom.php';
 if (isset($_GET['putDate'])) {
     $dateIn = $_GET['putDate'];
@@ -9,16 +10,8 @@ if (isset($_GET['putDate'])) {
 
 function getCurl($dateIn)
 {
-    $ch = curl_init();
-    $fp = fopen('parserFile.txt', 'w');
-    curl_setopt($ch, CURLOPT_URL, "http://katashi.ru/currency/?date=.$dateIn.");
-    curl_setopt($ch, CURLOPT_FILE, $fp);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:39.0) Gecko/20100101 Firefox/39.0 ');
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    $html = curl_exec($ch);
-    curl_close($ch);
-    $pattern='/<table(.*)\<\/table>/im';
-    $table=array();
-    preg_match_all($pattern,$html,$table);
-    print_r($table);
+    $data = file_get_html("http://katashi.ru/currency/?date=.$dateIn.");
+    foreach ($data->find('table#ratetable') as $element) {
+        echo "$element<br>";
     }
+}
--- test.php
+++ test.php
@@ -1,22 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf8'>
+<META name='description' CONTENT='Здесь описание для Вашей страницы..'>
+<META name='keywords' CONTENT='Здесь ключевые слова для Вашей страницы'>
+<title>Здесь впишите заголовок Вашей страницы..</title>
+</head>
+<body>
+
+<!-- Открываем PHP код -->
 <?php
-header('Content-Type: text/html; charset=utf-8');
+
+//Выводим наше изображение..
+$num=10;
+for ($i=1;$i<=$num;$i++){
+    echo '<img src="http://s.bash.im/img/yne025qxsecurafm428233.png">';
+}
+
+
+/*header('Content-Type: text/html; charset=utf-8');
 include_once 'simple_html_dom.php';
 $dateIn = '31/07/2015';
-/*
-$ch = curl_init();
-$fp = fopen('parserFile.txt', 'w');
-curl_setopt($ch, CURLOPT_URL, "http://katashi.ru/currency/?date=.$dateIn.");
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:39.0) Gecko/20100101 Firefox/39.0 ');
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$html = curl_exec($ch);
-curl_close($ch);
-$pattern='|<table(.+)</table>|';
-preg_match($pattern,$html,$table);
-print_r($table);*/
 $data = file_get_html("http://katashi.ru/currency/?date=.$dateIn.");
 foreach ($data->find('h1') as $element) {
     echo "$element<br>";
 };
-//echo $es;
+*/
+
+?>
+
+</body>
+</html>
+
+
 
