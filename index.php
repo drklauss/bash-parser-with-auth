959e7a8d38219857179a9ccd0421b82ddeb67994
Aleksey <dr.klauss@gmail.com>
2015-07-31 10:21:32 +0300

    firrst serious commit

--- index.php
+++ index.php
@@ -5,4 +5,13 @@
  * Date: 31.07.2015
  * Time: 9:58
  */
+
 $ch=curl_init('http://www.cbr.ru/currency_base/daily.aspx?date_req=31.07.2015');
+$fp=fopen('parserFile.txt','w');
+
+curl_setopt($ch,CURLOPT_FILE,$fp);
+curl_setopt($ch,CURLOPT_HEADER,0);
+
+curl_exec($ch);
+curl_close($ch);
+fclose($fp);
