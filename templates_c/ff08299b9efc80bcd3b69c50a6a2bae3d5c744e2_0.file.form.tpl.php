09e5f4726d57efddf4b664069517136217252d60
Aleksey <dr.klauss@gmail.com>
2015-08-03 17:04:02 +0300

    commit for happiness

--- templates_c/ff08299b9efc80bcd3b69c50a6a2bae3d5c744e2_0.file.form.tpl.php
+++ templates_c/ff08299b9efc80bcd3b69c50a6a2bae3d5c744e2_0.file.form.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2015-08-03 14:46:04
+<?php /* Smarty version 3.1.27, created on 2015-08-03 18:03:01
          compiled from "P:\home\test1.ru\www\test_bash\templates\form.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:2393955bf466c272186_15309305%%*/
+/*%%SmartyHeaderCode:3110255bf74955e3fa3_06938153%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,26 +9,26 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ff08299b9efc80bcd3b69c50a6a2bae3d5c744e2' => 
     array (
       0 => 'P:\\home\\test1.ru\\www\\test_bash\\templates\\form.tpl',
-      1 => 1438598626,
+      1 => 1438606461,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '2393955bf466c272186_15309305',
+  'nocache_hash' => '3110255bf74955e3fa3_06938153',
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_55bf466c2b7c02_30211063',
+  'unifunc' => 'content_55bf749562cea5_43162812',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_55bf466c2b7c02_30211063')) {
-function content_55bf466c2b7c02_30211063 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_55bf749562cea5_43162812')) {
+function content_55bf749562cea5_43162812 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '2393955bf466c272186_15309305';
+$_smarty_tpl->properties['nocache_hash'] = '3110255bf74955e3fa3_06938153';
 ?>
 <html>
 <head>
     <title>Парсер для Баша</title>
 </head>
-<h2>Парсер получения баша</h2>
+<h2>Парсер получения цитат с bash.im</h2>
 <form name="getDateFrom" method="get" action="/test_bash/index.php">
     <label>Начальная страница: <input type="text" name="grubPageStart" placeholder="Номер страницы"></label>
     <label>Конечная страница: <input type="text" name="grubPageFinish" placeholder="Номер страницы"></label>
